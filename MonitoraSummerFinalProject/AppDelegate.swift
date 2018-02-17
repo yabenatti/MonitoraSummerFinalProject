c22f98a75992ed1c290e05f6551199c362192680
yabenatti <min.benatti@gmail.com>
2018-02-17 17:53:59 -0200

    Updating colors

--- MonitoraSummerFinalProject/AppDelegate.swift
+++ MonitoraSummerFinalProject/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = ColorUtils.summerLeadBlue
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor : ColorUtils.summerLeadBlue]
+        UINavigationBar.appearance().tintColor = ColorUtils.summerLeadBlue
+        
         return true
     }
 
---
+++ MonitoraSummerFinalProject/Utils/ColorUtils.swift
@@ -0,0 +1,17 @@
+//
+//  ColorUtils.swift
+//  MonitoraSummerFinalProject
+//
+//  Created by Yasmin Benatti on 2018-02-17.
+//  Copyright © 2018 Yasmin Benatti. All rights reserved.
+//
+
+import UIKit
+
+class ColorUtils {
+    
+    private init () {}
+    
+    static let summerYellow : UIColor! = UIColor(red: 241/255, green: 196/255, blue: 0/255, alpha: 1)
+    static let summerLeadBlue : UIColor! = UIColor(red: 36/255, green: 59/255, blue: 84/255, alpha: 1)
+}
